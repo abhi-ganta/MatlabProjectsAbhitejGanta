143ce15969f978878949cf017006e9235548b8f4
abhi-ganta <31359843+abhi-ganta@users.noreply.github.com>
2017-12-25 18:33:56 -0500

    edit 4

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -5,6 +5,8 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+alpha = 0.01;
+
 
 % You need to return the following variables correctly 
 J = 0;
@@ -12,7 +14,8 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-J = (1./(2.*m)).*((theta'.* X) - y).^2;
+
+J = (1/(2*m)).*sum(((theta'.* X) - y).^2);
 
 
 
